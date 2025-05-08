f9d0352db33d41cfbf25a6ca902599112082de30
yeodojun <kjr0118@naver.com>
2025-05-09 05:22:41 +0900

    Test

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,21 +3,53 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 5f;
+    public float jumpForce = 7f;
+    public float JumpEndDelay = 0.06f;
     private Rigidbody2D rb;
     private float horizontalInput;
+    Animator animator;
+    private bool isGrounded = false;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
     }
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(horizontalInput * speed, rb.linearVelocity.y);
     }
+    void Jump()
+    {
+        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        animator.SetTrigger("Jump");
+        Invoke(nameof(EndJump), JumpEndDelay);
+    }
+    private void EndJump()
+    {
+        animator.SetTrigger("End");
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
 
-
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
 }
